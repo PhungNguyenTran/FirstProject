442ac44cf8d6b1ad36d05d256ff85772c920e482
Phụng Nguyễn <phungnguyen@Phungs-MacBook-Pro.local>
2022-12-25 22:16:36 +0700

    update code

--- FirstProject/Persistence/DbContext/FirstDbContext.cs
+++ FirstProject/Persistence/DbContext/FirstDbContext.cs
@@ -6,12 +6,21 @@ namespace Persistence.Context
 {
     public class FirstDbContext : DbContext
     {
+        public FirstDbContext()
+        {
+
+        }
         public FirstDbContext(DbContextOptions options) : base(options) { }
         public DbSet<Person> People { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             new PersonConfiguration().Configure(modelBuilder.Entity<Person>());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Data Source=localhost,1433;Initial Catalog=EFDatabase;User Id=sa;Password=Password123@;");
         }
     }
 }
